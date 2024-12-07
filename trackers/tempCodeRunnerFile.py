519740f873f647b56e91e28e7eab75e11c2a0772
Henrique Souza <102422550+duows@users.noreply.github.com>
2024-12-07 16:07:54 -0300

    new files !

---
+++ trackers/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+def add_position_to_tracks(sekf,tracks):
+    #     for object, object_tracks in tracks.items():
+    #         for frame_num, track in enumerate(object_tracks):
+    #             for track_id, track_info in track.items():
+    #                 bbox = track_info['bbox']
+    #                 if object == 'ball':
+    #                     position= get_center_of_bbox(bbox)
+    #                 else:
+    #                     position = get_foot_position(bbox)
+    #                 tracks[object][frame_num][track_id]['position'] = position
